e54af9904fad63141aaf2f4e9e753de364e06a5b
Berat <kurtaranberat891@gmail.com>
2024-03-23 21:50:04 +0300

    Place Detail kısmı

---
+++ mekanAPP/app/Components/PlaceDetail/PlaceDetail.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PlaceDetail() {
+  return (
+    <View>
+      <Text>PlaceDetail</Text>
+    </View>
+  )
+}
