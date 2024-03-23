9e77b14f61bfd35401e613a8afe65b72b602404c
Berat <kurtaranberat891@gmail.com>
2024-03-23 21:52:13 +0300

    Search componenti hazırlama

---
+++ mekanAPP/app/Components/Search/BusinessItem.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function BusinessItem() {
+  return (
+    <View>
+      <Text>BusinessItem</Text>
+    </View>
+  )
+}
